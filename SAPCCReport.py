8d3065bb985f3453d2d53005c8574652278d0717
Chris McIntyre <chris.mcintyre@fiskars.com>
2025-09-26 11:05:21 -0400

    initial commit

---
+++ SAPCCReport.py
@@ -0,0 +1,38 @@
+import openpyxl as pyxl
+from openpyxl.worksheet.worksheet import Worksheet
+from abc import ABC
+from dataclasses import dataclass
+from pathlib import Path
+
+''' An absctract class to represent a SAP CC Excel export report'''
+
+@dataclass
+class SAPCCReportFile:
+    report_path:Path
+    report_workbook:pyxl.Workbook
+
+def reprt_file_from_path(report_path: Path) -> SAPCCReportFile:
+    wb = pyxl.load_workbook(report_path.as_posix())
+    return SAPCCReportFile(report_path, wb)
+
+class InvalidSAPCCReport(Exception):
+    pass
+
+class SAPCCExportReport(ABC):
+    def __init__(self, report_file: SAPCCReportFile):
+        self._report_file = report_file
+
+
+    @property
+    def wb(self) -> pyxl.Workbook:
+        return self._report_file.report_workbook
+    
+    @property
+    def data_starting_row(self) -> int:
+        return 4
+    
+    def _get_ws(self, report_file:SAPCCReportFile, ws_index:int=-1) -> Worksheet:
+        return report_file.report_workbook._sheets[ws_index] #type:ignore
+    
+
+
