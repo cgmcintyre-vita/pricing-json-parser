ca479b31fc71d37f237e68c7d45d2638686e652a
Chris McIntyre <chris.mcintyre@fiskars.com>
2025-09-26 15:51:46 -0400

    Fixed the handling of accessing worksheet

--- SAPCCReport.py
+++ SAPCCReport.py
@@ -3,8 +3,10 @@ from openpyxl.worksheet.worksheet import Worksheet
 from abc import ABC
 from dataclasses import dataclass
 from pathlib import Path
+from typing import cast
 
 ''' An absctract class to represent a SAP CC Excel export report'''
+PRIMARY_SHEETNAME:str = "CommerceProduct"
 
 @dataclass
 class SAPCCReportFile:
@@ -19,6 +21,7 @@ class InvalidSAPCCReport(Exception):
     pass
 
 class SAPCCExportReport(ABC):
+    
     def __init__(self, report_file: SAPCCReportFile):
         self._report_file = report_file
 
@@ -31,8 +34,10 @@ class SAPCCExportReport(ABC):
     def data_starting_row(self) -> int:
         return 4
     
-    def _get_ws(self, report_file:SAPCCReportFile, ws_index:int=-1) -> Worksheet:
-        return report_file.report_workbook._sheets[ws_index] #type:ignore
+    def _get_ws(self, report_file:SAPCCReportFile, sheetname:str=PRIMARY_SHEETNAME) -> Worksheet:
+        return report_file.report_workbook[sheetname]
+    
+
     
 
 
